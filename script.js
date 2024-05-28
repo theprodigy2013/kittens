3687b720f8e811b7cd86ac0b2b7807917b549942
theprodigy2013 <150455861+theprodigy2013@users.noreply.github.com>
2024-05-28 19:27:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,42 @@ function displayGreeting() {
     var greetingMessage = document.getElementById('greetingMessage');
     var customImage = document.getElementById('customImage');
     var images = [
-        'assets/image1.jpg',
-        'assets/image2.jpg',
-        'assets/image3.jpg'
+        'assets/1.jpg',
+        'assets/2.jpg',
+        'assets/3.jpg',
+        'assets/4.jpg',
+        'assets/5.jpg',
+        'assets/6.jpg',
+        'assets/7.jpg',
+        'assets/8.jpg',
+        'assets/9.jpg',
+        'assets/10.jpg',
+        'assets/11.jpg',
+        'assets/12.jpg',
+        'assets/13.jpg',
+        'assets/14.jpg',
+        'assets/15.jpg',
+        'assets/16.jpg',
+        'assets/17.jpg',
+        'assets/18.jpg',
+        'assets/19.jpg',
+        'assets/20.jpg',
+        'assets/21.jpg',
+        'assets/22.jpg',
+        'assets/23.jpg',
+        'assets/24.jpg',
+        'assets/25.jpg',
+        'assets/26.jpg',
+        'assets/27.jpg',
+        'assets/28.jpg',
+        'assets/29.jpg',
+        'assets/30.jpg',
+        'assets/31.jpg',
+        'assets/32.jpg',
+        'assets/33.jpg',
+        'assets/34.jpg',
+        'assets/35.jpg',
+        'assets/36.jpg'
     ];
     var randomImage = images[Math.floor(Math.random() * images.length)];
     
