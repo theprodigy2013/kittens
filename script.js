2613ea3416ceb9498025120bd027b07b02169950
theprodigy2013 <150455861+theprodigy2013@users.noreply.github.com>
2024-06-04 08:32:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,39 +3,15 @@ function displayGreeting() {
     var greetingMessage = document.getElementById('greetingMessage');
     var customImage = document.getElementById('customImage');
     var images = [
-        'assets/1.gif',
-        'assets/2.jpg',
-        'assets/3.jpg',
-        'assets/4.gif',
-        'assets/6.png',
-        'assets/7.png',
-        'assets/8.png',
-        'assets/9.png',
-        'assets/11.png',
-        'assets/12.png',
-        'assets/13.png',
-        'assets/14.jpg',
-        'assets/15.jpg',
-        'assets/16.jpg',
-        'assets/17.jpg',
-        'assets/19.png',
-        'assets/20.png',
-        'assets/21.png',
-        'assets/22.jpg',
-        'assets/23.jpg',
-        'assets/24.jpg',
-        'assets/25.jpg',
-        'assets/26.jpg',
-        'assets/27.jpg',
-        'assets/28.jpg',
-        'assets/29.gif',
-        'assets/30.jpg',
-        'assets/31.png',
-        'assets/32.png',
-        'assets/33.jpeg',
-        'assets/34.gif',
-        'assets/35.jpg',
-        'assets/36.png'
+        'web/1.jpg',
+        'web/2.jpg',
+        'web/3.jpg',
+        'web/4.jpg',
+        'web/5.jpg',
+        'web/6.jpg',
+        'web/7.jpg',
+        'web/8.jpg',
+        'web/9.jpg'        
     ];
     var randomImage = images[Math.floor(Math.random() * images.length)];
     
