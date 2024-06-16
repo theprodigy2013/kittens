59d39c4302b50a10b35da4f10358f873742187f7
theprodigy2013 <150455861+theprodigy2013@users.noreply.github.com>
2024-06-16 21:53:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function displayGreeting() {
     var name = document.getElementById('nameInput').value;
     var greetingMessage = document.getElementById('greetingMessage');
     var customImage = document.getElementById('customImage');
+    var redirectButton = document.getElementById('redirectButton');
     var images = [
         'web/1.jpg',
         'web/2.jpg',
@@ -10,16 +11,23 @@ function displayGreeting() {
         'web/5.jpg',
         'web/6.jpg',
         'web/8.jpg',
-        'web/9.jpg'        
+        'web/9.jpg'
     ];
     var randomImage = images[Math.floor(Math.random() * images.length)];
     
-    if(name) {
+    if (name) {
         greetingMessage.textContent = "Here's what awaits your baby, " + name + ":";
         customImage.src = randomImage;
         customImage.style.display = 'block';
+        customImage.onload = function() {
+            redirectButton.style.display = 'inline-block';
+        };
     } else {
         greetingMessage.textContent = "Your name first.";
         customImage.style.display = 'none';
+        redirectButton.style.display = 'none';
     }
 }
+document.getElementById('redirectButton').addEventListener('click', function() {
+    window.location.href = 'https://docs.google.com/forms/d/your_custom_form_id'; 
+});
